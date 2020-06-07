f50aa0e8c95018979f493db2fc81b94a2cae0be1
Luis Carlos <luis.daza.consultant@sothebys.com>
2020-06-07 16:37:00 -0500

    Create constants

---
+++ src/main/java/com/daza/code/util/Constant.java
@@ -0,0 +1,14 @@
+package com.daza.code.util;
+
+public class Constant {
+
+  private Constant(){}
+
+  public static final String INPUT_FOLDER = "input-files";
+  public static final String OUTPUT_FOLDER = "output-files";
+  public static final String INPUT_FILE = "in";
+  public static final String OUTPUT_FILE = "out";
+  public static final String TXT = ".txt";
+  public static final int MAX_AMOUNT_OF_LUNCHES = 3;
+  public static final int MAX_AMOUNT_OF_BLOCKS = 10;
+}
